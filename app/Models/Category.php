3096187dcf1d232facc0280e1c8b3c0bde96a1b0
mugiaoo <mugiaoomyun@gmail.com>
2024-08-16 06:13:30 +0900

    カテゴリごとに分けて表示

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,4 +14,8 @@ class Category extends Model
     public function posts(){
         return $this->hasMany(Post::class);
     }
+    
+    public function getByCategory(int $limit_count = 5){
+        return $this->posts()->with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
