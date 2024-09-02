97a961c5673d02253d061a6a38bfeb254fd8b1f8
mugiaoo <mugiaoomyun@gmail.com>
2024-09-02 22:22:19 +0900

    Breeze認証機能の実装

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
